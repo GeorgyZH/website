815e7dcb97ff60ff4ff04449d0de7594b31f36af
notba <notba@DESKTOP-ADFR5S2>
2023-03-01 17:02:52 +0300

    Добавьте файлы проекта.

---
+++ Controllers/handlerController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebSite.Models;
+
+namespace WebSite.Controllers
+{
+    public class handlerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Check(IndexModel indexModel)
+        {
+            if (ModelState.IsValid)
+                    return View("AfterRegistr");
+
+            return View("AfterRegistr");
+        }
+    }
+}
---
+++ Models/IndexModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebSite.Models
+{
+    public class IndexModel
+    {
+        public string Email { get; set; }
+        public string Pass { get; set; }
+
+    }
+}
